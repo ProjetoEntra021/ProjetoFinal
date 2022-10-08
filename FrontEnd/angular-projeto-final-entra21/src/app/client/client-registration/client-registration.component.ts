8406e71dccf7959ee5384fbe5d4adf5a6ad7cf74
pablompamplona <pablompamplona@gmail.com>
2022-10-08 00:44:43 -0300

    save client with address works in backend, fk dont works in front

--- FrontEnd/angular-projeto-final-entra21/src/app/client/client-registration/client-registration.component.ts
+++ FrontEnd/angular-projeto-final-entra21/src/app/client/client-registration/client-registration.component.ts
@@ -1,12 +1,14 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ClientService } from '../../service/client.service';
-import { Client } from '../../shared/model/client';
-import { Location } from '@angular/common';
-import { Address } from '../../shared/model/address';
-import { Contact } from '../../shared/model/contact';
 import { AddressService } from 'src/app/service/address.service';
+
+import { ClientService } from '../../service/client.service';
+import { Address } from '../../shared/model/address';
+
+import { Contact } from '../../shared/model/contact';
+import { Client } from './../../shared/model/client';
 
 @Component({
   selector: 'app-client-registration',
@@ -15,15 +17,7 @@ import { AddressService } from 'src/app/service/address.service';
 })
 export class ClientRegistrationComponent implements OnInit {
 
-  clientForm = this.formBuilder.group({
-    name: ['', Validators.required],
-    birthDate: ['', Validators.required],
-    cpf: ['', Validators.required],
-    cnh: ['', Validators.required],
-    gender: ['', Validators.required],
-    })
-
-    addressForm = this.formBuilder.group ({
+  addressForm = this.formBuilder.group ({
     cep: ['', Validators.required],
     street: ['', Validators.required],
     number: ['', Validators.required],
@@ -34,8 +28,20 @@ export class ClientRegistrationComponent implements OnInit {
 
   })
 
+  clientForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    birthDate: ['', Validators.required],
+    cpf: ['', Validators.required],
+    cnh: ['', Validators.required],
+    gender: ['', Validators.required],
+    // addresses: [this.formBuilder.group(this.addressForm)]
+
+  });
+
+
+
   public client!: Client;
-  public address!: Address;
+  public address: Partial<Address> = this.addressForm.value
   public contact!: Contact;
 
 
@@ -54,18 +60,24 @@ export class ClientRegistrationComponent implements OnInit {
 
 
   addClient() {
-    this.clientService.save(this.clientForm.value).subscribe(
-       {
-        next: () => this.onSuccess(),
-        error: (e) => this.onError()
-      }
-    )
-    this.address.client = this.client;
-    this.addressService.save(this.addressForm.value).subscribe({
-        next: () => this.onSuccess(),
-        error: (e) => this.onError()
-    })
+    this.client.addresses.push(this.address)
+    this.clientService.save(this.clientForm.value).subscribe({
+      next: () => this.onSuccess(),
+      error: (e) => this.onError()
+    });
   }
+
+
+
+
+  // private addAddress() {
+
+  //   this.addressService.save(this.addressForm.value).pipe(
+
+  //   )
+
+
+  // }
 
   private onSuccess() {
     this.snackBar.open('Cliente cadastrado com sucesso!', '', { duration: 3000 })
