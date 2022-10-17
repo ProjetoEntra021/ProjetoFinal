f0da0a634461d59089f97f94e18ec6c3023cef94
Mateus Bruscato <mateus.bruscato@gmail.com>
2022-10-17 18:59:23 -0300

    Rental list and payment list updated. Payment controller created and update paymentStatus method

--- BackEnd/src/main/java/com/entra21/Entra21ProjetoFinalApplication.java
+++ BackEnd/src/main/java/com/entra21/Entra21ProjetoFinalApplication.java
@@ -4,8 +4,10 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Entra21ProjetoFinalApplication {
 
 	public static void main(String[] args) {
