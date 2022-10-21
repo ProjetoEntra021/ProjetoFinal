bfe7b7b63e3bfb0d1ea82dbf6d8a28b0820db725
Wolleck <vifewo@hotmail.com>
2022-10-20 21:02:21 -0300

    created authenticate method

--- BackEnd/src/main/java/com/entra21/security/CustomUserDetailsService.java
+++ BackEnd/src/main/java/com/entra21/security/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import com.entra21.entities.User;
@@ -14,18 +15,34 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	UserRepository userRepository;
-	
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		
-	User existsUser = userRepository.findByUsername(username);	
-		
-		if (existsUser != null) {
+
+		User existsUser = userRepository.findByUsername(username);
+
+		if (existsUser == null) {
 			throw new Error("User does not exists!");
 		}
 		return UserPrincipal.create(existsUser);
 	}
 
+	public boolean Authenticate(String username, String password) {
+
+		User existsUser = userRepository.findByUsername(username);
+
+		if (existsUser == null) {
+			throw new Error("User does not exists!");
+		} else if (existsUser.getUsername() == username) {
+			existsUser.setUsername(username);
+			if (BCrypt.checkpw(password, existsUser.getPassword())) {
+				// Enviar para o front junto do id e do username
+				return true;
+			} else {
+				throw new Error("Password dont matches");
+
+			}
+		}
+		return false;
+	}
 }
